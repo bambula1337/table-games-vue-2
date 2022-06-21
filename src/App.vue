6e0533a8b5660e6faae2bbc5ff80a2daf3ddc8f7
adms <adms@pop-os.localdomain>
2022-06-21 14:21:08 +0300

    Initialized project

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
     <router-view />
   </div>
 </template>
+
+<script>
+import "@/assets/styles/tailwind/tailwind.scss";
+
+export default {
+  name: "App",
+};
+</script>
 
 <style lang="scss">
 #app {
@@ -21,8 +25,7 @@ nav {
   padding: 30px;
 
   a {
-    font-weight: bold;
-    color: #2c3e50;
+    @apply flex justify-center text-red-900 text-2xl;
 
     &.router-link-exact-active {
       color: #42b983;
