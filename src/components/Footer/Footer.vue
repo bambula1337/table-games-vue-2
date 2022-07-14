97386540b14e1096c6491343acead7e21089276f
adms <adms@pop-os.localdomain>
2022-07-14 16:00:01 +0300

    rwd update - header state 2/2

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -21,7 +21,14 @@
             {{ link.name }}
           </p>
         </div>
+        <div class="delivery-wrapper">
+          <p class="link">Оплата и доставка</p>
+          <p class="link">Гарантия и возврат</p>
+        </div>
         <div class="information">
+          <button-orange-default class="contact-button"
+            ><p class="button-text">Заказать звонок</p></button-orange-default
+          >
           <div class="information-location-wrapper">
             <p class="information-location">
               {{ information.address }}
@@ -53,13 +60,34 @@
             Пользовательское соглашение © 2021 MagicGoldFish.ru
           </p>
         </div>
+        <div class="bottom-information-pc">
+          <p class="privacy privacy-left">
+            © 2021 MagicGoldFish.ru
+            <span class="link">Политика конфиденциальности</span>
+          </p>
+          <div class="partners">
+            <img
+              alt=""
+              class="partner"
+              v-for="partner in partners"
+              :key="partner.id"
+              :src="srcFixer(partner.image.url)"
+            />
+          </div>
+          <p class="privacy privacy-right">
+            Содержимое не является публичной офертой
+            <span class="link">Пользовательское соглашение</span>
+          </p>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import ButtonOrangeDefault from "../Global/Buttons/ButtonOrangeDefault.vue";
 export default {
+  components: { ButtonOrangeDefault },
   name: "FooterComponent",
   data() {
     return {
@@ -159,6 +187,29 @@ export default {
           url: "",
         },
       ],
+      partners: [
+        {
+          id: 1,
+          name: "",
+          image: {
+            url: "images/partners/partner_1.svg",
+          },
+        },
+        {
+          id: 2,
+          name: "",
+          image: {
+            url: "images/partners/partner_2.svg",
+          },
+        },
+        {
+          id: 3,
+          name: "",
+          image: {
+            url: "images/partners/partner_3.svg",
+          },
+        },
+      ],
     };
   },
   methods: {
@@ -175,16 +226,21 @@ export default {
 }
 .footer {
   @apply w-full flex flex-col bg-project-black px-5 pt-10 pb-7;
-  @apply TM:max-w-6xl;
+  @apply TM:max-w-6xl TM:pt-14;
 
   & .top-container {
     @apply flex flex-col items-center text-center;
-    @apply TM:flex-row TM:justify-between TM:items-start;
+    @apply TM:flex-row TM:justify-between TM:items-start TM:pb-22;
     & .logo-wrapper {
       @apply w-32 h-8 mb-7 text-left;
-      @apply TM:w-56;
+      @apply TM:w-41;
+      @apply TL:w-56;
       & .logo {
-        @apply TM:w-38;
+        @apply TM:w-34 TM:mb-4 transition-opacity duration-300 cursor-pointer;
+        @apply TL:w-38;
+        &:hover {
+          @apply opacity-70;
+        }
       }
       & .address {
         @apply hidden text-white w-54;
@@ -195,6 +251,7 @@ export default {
       & .link {
         @apply flex flex-col items-center text-white font-bold select-none cursor-pointer mb-2;
         @apply TM:items-start;
+        @apply TL:text-lg;
         &::after {
           @apply w-0 transition-all duration-300 -mt-0.5;
           content: "";
@@ -208,9 +265,9 @@ export default {
       }
       & .catalog-links {
         @apply hidden items-start;
-        @apply TM:flex TM:flex-col;
+        @apply TM:flex TM:flex-col TM:w-38 TM:text-left;
         & .link {
-          @apply mb-1 font-normal transition-all duration-300;
+          @apply mb-0 font-normal transition-all duration-300 text-base;
           &:hover {
             @apply opacity-70;
             &::after {
@@ -222,10 +279,28 @@ export default {
     }
     & .links-wrapper {
       @apply h-40 flex flex-col justify-between items-center mb-6;
-      @apply TM:items-start;
+      @apply TM:items-start TM:h-40;
       & .link {
         @apply flex flex-col items-center text-white font-bold select-none cursor-pointer;
         @apply TM:items-start;
+        @apply TL:text-lg;
+        &::after {
+          @apply w-0 transition-all duration-300 -mt-0.5;
+          content: "";
+          border-top: 2px white solid;
+        }
+        &:hover {
+          &::after {
+            @apply LS:w-1/2;
+          }
+        }
+      }
+    }
+    & .delivery-wrapper {
+      @apply text-left text-white font-bold;
+      & .link {
+        @apply flex flex-col items-start text-white font-bold select-none cursor-pointer mb-2.5;
+        @apply TL:text-lg TL:mb-1;
         &::after {
           @apply w-0 transition-all duration-300 -mt-0.5;
           content: "";
@@ -241,6 +316,15 @@ export default {
     & .information {
       @apply flex flex-col items-center;
       @apply TM:items-end TM:text-right;
+      & .contact-button {
+        @apply hidden w-52 h-13 bg-none border-project-orange text-project-orange text-lg mb-3;
+        border-width: 1px;
+        @apply TM:block;
+        @apply TL:w-56;
+        & .button-text {
+          @apply h-13 flex justify-center items-center;
+        }
+      }
     }
     & .information-location-wrapper {
       @apply w-56 mb-4;
@@ -272,10 +356,42 @@ export default {
     }
   }
   & .bottom-container {
+    @apply relative;
+    @apply TM:pt-7;
+    &::before {
+      @apply absolute w-full h-10 opacity-70 bottom-6;
+      border-top: 2px gray solid;
+      content: "";
+    }
     & .bottom-information-mobile {
       @apply text-center;
+      @apply TM:hidden;
       & .information-text {
         color: rgba(255, 255, 255, 0.44);
+      }
+    }
+    & .bottom-information-pc {
+      @apply hidden w-full text-project-gray opacity-50 justify-between;
+      @apply TM:flex;
+      & .privacy {
+        @apply text-sm;
+        & .link {
+          @apply underline cursor-pointer;
+        }
+      }
+      & .privacy-left {
+        @apply w-54;
+      }
+      & .privacy-right {
+        @apply text-right;
+        @apply w-76;
+      }
+      & .partners {
+        @apply flex self-center absolute;
+        left: 41.5%;
+        & .partner {
+          @apply h-5 mr-2;
+        }
       }
     }
   }
